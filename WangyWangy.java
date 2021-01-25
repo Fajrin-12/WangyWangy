f08bca70392991750ca986f0f142d44920904713
Fajrin-12 <75468257+Fajrin-12@users.noreply.github.com>
2021-01-25 15:25:16 +0700

    Add files via upload

---
+++ WangyWangy.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wangywangy;
+import java.util.Scanner;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class WangyWangy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        String KEQING;
+        System.out.println("Masukkan nama yang anda ingin WANGY WANGY : ");
+        KEQING=input.nextLine();
+        
+        System.out.println(KEQING+"...... "+KEQING+" ❤ ❤ ❤ WANGY WANGY WANGY WANGY WANGY WANGY HU HA HU HA HU HA, aaaah baunya "+KEQING+" wangi aku mau nyiumin aroma wanginya "+KEQING+" AAAAAAAAH ~~ Rambutnya.... aaah rambutnya juga pengen aku elus-elus ~ AAAAAH "+KEQING+" ❤ ❤ ❤ manis banget AAAAAAAAH\n" +
+"TATAPAN "+KEQING+" BEGITU MENGGODAAAAAAAAA............ GUA RELA JADI BUDAK SIMP HANYA DEMI"+KEQING+" TERDJINTA AAAAAAAAAAAAAAAAAAAAAAAAAAAAAGH\n" +
+"apa ? "+KEQING+" itu gak nyata ? Cuma karakter 2 dimensi katamu ? nggak, ngak ngak ngak ngak NGAAAAAAAAK GUA GAK PERCAYA ITU DIA NYATA !! GUA GAK PEDULI SAMA KENYATAAN POKOKNYA GAK PEDULI. "+KEQING+" ngeliat gw ... "+KEQING+" NGELIATIN GW! "+KEQING+"... kamu percaya sama aku ? aaaaaaaaaaah syukur "+KEQING+" gak malu memiliki aku aaaaaah ❤ ❤ ❤ YEAAAAAAAAAAAH GUA MASIH PUNYA "+KEQING+" ,"+KEQING+" AKU SAYANG "+KEQING+" AKU CINTA "+KEQING+" AKU AKU INGIN "+KEQING+" MENJADI BIDADARIKUUUUUUU!!!!!!!!!!!!!\n" +
+"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAGH!!!!!!!!!!!!!!!!!!!!!!");
+        
+    
+}
+}
